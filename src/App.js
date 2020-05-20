2f02958deb7805a0e99b7c2f4bce41a8a1bd3ac7
Sajitha Kunnath <sajitha.kunnath@interfacesys.com>
2020-05-20 18:39:56 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ContactList from "./ContactList.js";
+import contacts from "./Contact.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section id="contact-list">
+      <h1>Contacts</h1>
+      <ContactList contacts={contacts}></ContactList>
+    </section>
   );
 }
 
